802cc0000e77a57e6aefef3a1cecbe8dbefd43f5
pontefabio <79936776+pontefabio@users.noreply.github.com>
2024-04-03 19:07:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,3 +36,6 @@ def predict_api():
 
 if __name__ == "__main__":
     app.run(debug=True)
+# Use the PORT environment variable provided by Heroku
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
